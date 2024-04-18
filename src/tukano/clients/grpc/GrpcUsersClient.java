c8908a3c93943747ccac6c900d55a5aefb4716e3
José Trigueiro <joseleonardo2000@gmail.com>
2024-04-18 17:46:23 +0100

    grpc meio feito

--- src/tukano/clients/grpc/GrpcUsersClient.java
+++ src/tukano/clients/grpc/GrpcUsersClient.java
@@ -1,46 +1,92 @@
 package tukano.clients.grpc;
 
 import java.net.URI;
+import java.util.ArrayList;
 import java.util.List;
-
-import tukano.api.User;
+import java.util.concurrent.TimeUnit;
+import io.grpc.Status;
+import io.grpc.Status.Code;
+import io.grpc.StatusRuntimeException;
 import tukano.api.java.Result;
+import tukano.api.java.Result.ErrorCode;
+import io.grpc.ManagedChannelBuilder;
+import tukano.api.User;
 import tukano.api.java.Users;
+import tukano.impl.grpc.generated_java.UsersGrpc;
+import tukano.impl.grpc.generated_java.UsersProtoBuf.*;
+import static tukano.impl.grpc.common.DataModelAdaptor.User_to_GrpcUser;
+import static tukano.impl.grpc.common.DataModelAdaptor.GrpcUser_to_User;
+import java.util.function.Supplier;
+import static tukano.api.java.Result.error;
+import static tukano.api.java.Result.ok;
 
 public class GrpcUsersClient implements Users {
 
-    public GrpcUsersClient(URI serverURI) {
+    private static final long GRPC_REQUEST_TIMEOUT = 5000;
+    final UsersGrpc.UsersBlockingStub stub;
 
+    public GrpcUsersClient(URI serverURI) {
+        var channel = ManagedChannelBuilder.forAddress(serverURI.getHost(), serverURI.getPort()).usePlaintext().build();
+        stub = UsersGrpc.newBlockingStub(channel).withDeadlineAfter(GRPC_REQUEST_TIMEOUT, TimeUnit.MILLISECONDS);
     }
 
     @Override
     public Result<String> createUser(User user) {
-        // TODO Auto-generated method stub
-        return null;
+        return toJavaResult(() -> {
+            var res = stub.createUser(CreateUserArgs.newBuilder()
+                    .setUser(User_to_GrpcUser(user))
+                    .build());
+            return res.getUserId();
+        });
     }
 
     @Override
     public Result<User> getUser(String userId, String pwd) {
-        // TODO Auto-generated method stub
-        return null;
+        return toJavaResult(() -> {
+            var res = stub.getUser(GetUserArgs.newBuilder()
+                    .setUserId(userId)
+                    .setPassword(pwd)
+                    .build());
+            return GrpcUser_to_User(res.getUser());
+        });
     }
 
     @Override
-    public Result<User> updateUser(String userId, String password, User user) {
-        // TODO Auto-generated method stub
-        return null;
+    public Result<User> updateUser(String userId, String pwd, User user) {
+        return toJavaResult(() -> {
+            var res = stub.updateUser(UpdateUserArgs.newBuilder()
+                    .setUserId(userId)
+                    .setPassword(pwd)
+                    .setUser(User_to_GrpcUser(user))
+                    .build());
+            return GrpcUser_to_User(res.getUser());
+        });
     }
 
     @Override
-    public Result<User> deleteUser(String userId, String password) {
-        // TODO Auto-generated method stub
-        return null;
+    public Result<User> deleteUser(String userId, String pwd) {
+        return toJavaResult(() -> {
+            var res = stub.deleteUser(DeleteUserArgs.newBuilder()
+                    .setUserId(userId)
+                    .setPassword(pwd)
+                    .build());
+            return GrpcUser_to_User(res.getUser());
+        });
     }
 
     @Override
     public Result<List<User>> searchUsers(String pattern) {
-        // TODO Auto-generated method stub
-        return null;
+        return toJavaResult(() -> {
+            var res = stub.searchUsers(SearchUserArgs.newBuilder()
+                    .setPattern(pattern)
+                    .build());
+
+            List<User> users = new ArrayList<>();
+            while (res.hasNext()) {
+                users.add(GrpcUser_to_User(res.next()));
+            }
+            return users;
+        });
     }
 
     @Override
@@ -51,8 +97,13 @@ public class GrpcUsersClient implements Users {
 
     @Override
     public Result<String> checkBlobId(String blobId) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'checkBlobId'");
+        return toJavaResult(() -> {
+            var res = stub.checkBlobId(CheckBlobId.newBuilder()
+                    .setBlobId(blobId)
+                    .build());
+
+            return res.getBlobId();
+        });
     }
 
     @Override
@@ -67,5 +118,27 @@ public class GrpcUsersClient implements Users {
         throw new UnsupportedOperationException("Unimplemented method 'deleteBlob'");
     }
 
-    
+    static <T> Result<T> toJavaResult(Supplier<T> func) {
+        try {
+            return ok(func.get());
+        } catch (StatusRuntimeException sre) {
+            var code = sre.getStatus().getCode();
+            if (code == Code.UNAVAILABLE || code == Code.DEADLINE_EXCEEDED)
+                throw sre;
+            return error(statusToErrorCode(sre.getStatus()));
+        }
+    }
+
+    static ErrorCode statusToErrorCode(Status status) {
+        return switch (status.getCode()) {
+            case OK -> ErrorCode.OK;
+            case NOT_FOUND -> ErrorCode.NOT_FOUND;
+            case ALREADY_EXISTS -> ErrorCode.CONFLICT;
+            case PERMISSION_DENIED -> ErrorCode.FORBIDDEN;
+            case INVALID_ARGUMENT -> ErrorCode.BAD_REQUEST;
+            case UNIMPLEMENTED -> ErrorCode.NOT_IMPLEMENTED;
+            default -> ErrorCode.INTERNAL_ERROR;
+        };
+    }
+
 }
---
+++ src/tukano/impl/grpc/common/DataModelAdaptor.java
@@ -0,0 +1,25 @@
+package tukano.impl.grpc.common;
+
+import tukano.api.User;
+import tukano.impl.grpc.generated_java.UsersProtoBuf.GrpcUser;
+
+public class DataModelAdaptor {
+
+	public static User GrpcUser_to_User(GrpcUser from) {
+		return new User(
+				from.getUserId(),
+				from.getPassword(),
+				from.getEmail(),
+				from.getDisplayName());
+	}
+
+	public static GrpcUser User_to_GrpcUser(User from) {
+		return GrpcUser.newBuilder()
+				.setUserId(from.getUserId())
+				.setPassword(from.getPwd())
+				.setEmail(from.getEmail())
+				.setDisplayName(from.getDisplayName())
+				.build();
+	}
+
+}
